1ea1570178a588c4f92f75a46c399ba615831690
MohammadKibria942 <yusefkibria942@gmail.com>
2023-05-01 20:27:34 +0100

    Third Addition

--- php-8.2.5/RecordSaver.php
+++ php-8.2.5/RecordSaver.php
@@ -1,6 +1,11 @@
 <?php
     	header('Access-Control-Allow-Origin: *');
-        $foreName = $_POST[forename];
+        $foreName = $_POST[foreName];
+        $surname = $_POST[surname];
+        $DOB = $_POST[DOB];
+        $genderCode = $_POST[genderCode];
+        $postCode = $_POST[postCode];
+
 
         $pdo = new \PDO("sqlite: vaccines.db");
         $st = $pdo->query("UPDATE patients SET where NHSNumber='".$nhsNo."'" );
